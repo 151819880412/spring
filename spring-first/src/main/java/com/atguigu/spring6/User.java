d959cd661779f6c23c6ced79ecd93fafcaedd144
Your Name <you@example.com>
2024-07-01 17:58:42 +0800

    集成日志

--- spring-first/src/main/java/com/atguigu/spring6/User.java
+++ spring-first/src/main/java/com/atguigu/spring6/User.java
@@ -1,4 +1,6 @@
 package com.atguigu.spring6;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @Author: Admin
@@ -9,12 +11,15 @@ package com.atguigu.spring6;
  * Description: 描述
  */
 public class User {
+    private Logger logger = LoggerFactory.getLogger(User.class);
+
     public void sayHello(){
         System.out.println("helloworld");
     }
 
     User() {
         System.out.println("User的无参构造方法");
+        logger.debug("Hello log4j2!");
     }
 
 }
